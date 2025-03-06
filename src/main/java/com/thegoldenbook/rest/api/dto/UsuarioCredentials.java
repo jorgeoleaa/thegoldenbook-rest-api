a86b4c2dba8fe1475e197f87c66d2f07a39f60f3
jorgeoleaa <jorgeoleacasanova@gmail.com>
2025-03-06 12:59:23 +0100

    Add class UsuarioCredentials

---
+++ src/main/java/com/thegoldenbook/rest/api/dto/UsuarioCredentials.java
@@ -0,0 +1,30 @@
+package com.thegoldenbook.rest.api.dto;
+
+public class UsuarioCredentials {
+	
+	private String mail;
+	private String password;
+	
+	public UsuarioCredentials() {
+		
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
