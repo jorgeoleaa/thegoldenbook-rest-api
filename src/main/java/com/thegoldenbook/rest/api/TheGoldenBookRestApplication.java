f4f141d3d2bda51fcddb2cb9ee586f6b7060b6e9
Jorge <jorgeoleacasanova@gmail.com>
2025-05-10 11:49:05 +0200

    refactor: translate class to english

--- src/main/java/com/thegoldenbook/rest/api/TheGoldenBookRestApplication.java
+++ src/main/java/com/thegoldenbook/rest/api/TheGoldenBookRestApplication.java
@@ -11,54 +11,50 @@ import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.servers.Server;
 import jakarta.ws.rs.ApplicationPath;
 
-//Nota profe para alumnado:
-//En Servlet 3.0, Application es una clase base proporcionada por JAX-RS 
-//(Java API for RESTful Web Services) para definir una aplicación RESTful 
-//sin necesidad de un web.xml.
-//ResourceConfig es una subclase de Jersey de Application para
-//configurar aspectos de jersey que antes iban en el web.xml
+// In Servlet 3.0, Application is a base class provided by JAX-RS 
+// (Java API for RESTful Web Services) to define a RESTful application 
+// without the need for a web.xml file.
+// ResourceConfig is a Jersey subclass of Application for
+// configuring Jersey aspects that used to be defined in web.xml.
 
 @OpenAPIDefinition(
-	    info = @Info(
-	        title = "The Golden Book API",
-	        version = "1.0",
-	        description = "API para negocio de venta de libros ",
-	        contact = @Contact(
-	            name = "Soporte API",
-	            email = "soporte@thegoldenbook.com",
-	            url = "https://thegoldenbook.com"
-	        ),
-	        license = @License(
-	            name = "MIT",
-	            url = "https://opensource.org/licenses/MIT"
-	        )
-	    ),
-	    servers = {
-	    		@Server(url = "http://localhost:8080/thegoldenbook-rest-api/", description = "Servidor Local")
-	    		// Cuando lo subais al hosting
-	    		// @Server(url = "https://api.thegoldenbook.com", description = "Servidor de Producción"),
+		info = @Info(
+				title = "The Golden Book API",
+				version = "1.0",
+				description = "API for a book-selling business",
+				contact = @Contact(
+						name = "API Support",
+						email = "support@thegoldenbook.com",
+						url = "https://thegoldenbook.com"
+						),
+				license = @License(
+						name = "MIT",
+						url = "https://opensource.org/licenses/MIT"
+						)
+				),
+		servers = {
+				@Server(url = "http://localhost:8080/thegoldenbook-rest-api/", description = "Local Server")
+				// When deploying to hosting
+				// @Server(url = "https://api.thegoldenbook.com", description = "Production Server"),
+		}
+		)
 
-	    }
-)
+@ApplicationPath("/api") // Base context for the endpoints
+public class TheGoldenBookRestApplication extends ResourceConfig {
 
-@ApplicationPath("/api") //contexto de base para los endpoints
-public class TheGoldenBookRestApplication extends ResourceConfig{
-	
 	public TheGoldenBookRestApplication() {
 		packages(TheGoldenBookRestApplication.class.getPackage().getName());
-		
-		//Anotaciones de Swagger para documentar con API
+
+		// Swagger annotations to document the API
 		register(io.swagger.v3.jaxrs2.integration.resources.OpenApiResource.class);
-		
+
 		register(DateParamConverterProvider.class);
-		
-		
-		// JAL: 
+
 		// https://github.com/swagger-api/swagger-ui?tab=readme-ov-file
-//		If you are looking for plain ol' HTML/JS/CSS, download the latest release and copy the contents of the /dist folder to your server.
-		// Y el link lleva a:
+		// If you are looking for plain ol' HTML/JS/CSS, download the latest release and copy the contents of the /dist folder to your server.
+		// The link points to:
 		// https://github.com/swagger-api/swagger-ui/releases/tag/v5.18.3
-		// Configurar en el swagger-initializer.js la URL de la web
-		// Abrir localhost:8080/<contexto>/swagger-ui
+		// Configure the API URL in swagger-initializer.js
+		// Open localhost:8080/<context>/swagger-ui
 	}
 }
