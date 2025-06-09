9a19b3079967a2ae649f31909c1df8c51137ed9e
jorgeoleaa <jorgeoleacasanova@gmail.com>
2025-06-09 10:33:22 +0200

    refactor: update get and set cache methods

--- src/main/java/com/thegoldenbook/rest/api/utils/RedisCache.java
+++ src/main/java/com/thegoldenbook/rest/api/utils/RedisCache.java
@@ -1,5 +1,7 @@
 package com.thegoldenbook.rest.api.utils;
 
+import com.google.gson.Gson;
+import java.lang.reflect.Type;
 import redis.clients.jedis.Jedis;
 
 public class RedisCache {
@@ -9,12 +11,15 @@ public class RedisCache {
 	
 	private static Jedis jedis = new Jedis(HOST, PORT);
 	
-	public static String get(String key) {
-		return jedis.get(key);
+
+	public static <T> T getObject(String key, Type type) {
+	    String json = jedis.get(key);
+	    return json != null ? new Gson().fromJson(json, type) : null;
 	}
 	
-	public static void set(String key, String value, int ttlSeconds) {
-		jedis.setex(key, ttlSeconds, value);
+	public static <T> void setObject(String key, T value, int ttlSeconds) {
+		String json = new Gson().toJson(value);
+		jedis.setex(key, ttlSeconds, json);
 	}
 	
 	public static void close() {
