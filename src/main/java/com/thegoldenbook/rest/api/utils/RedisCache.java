4da5bf02e97ff999d062ab56471847ed19dc105a
jorgeoleaa <jorgeoleacasanova@gmail.com>
2025-06-09 10:56:10 +0200

    refactor(cache): use JsonUtils for consistent Gson serialization in RedisCache

--- src/main/java/com/thegoldenbook/rest/api/utils/RedisCache.java
+++ src/main/java/com/thegoldenbook/rest/api/utils/RedisCache.java
@@ -1,6 +1,5 @@
 package com.thegoldenbook.rest.api.utils;
 
-import com.google.gson.Gson;
 import java.lang.reflect.Type;
 import redis.clients.jedis.Jedis;
 
@@ -12,14 +11,14 @@ public class RedisCache {
 	private static Jedis jedis = new Jedis(HOST, PORT);
 	
 
+	public static <T> void setObject(String key, T value, int ttlSeconds) {
+	    String json = JsonUtils.getGson().toJson(value);
+	    jedis.setex(key, ttlSeconds, json);
+	}
+
 	public static <T> T getObject(String key, Type type) {
 	    String json = jedis.get(key);
-	    return json != null ? new Gson().fromJson(json, type) : null;
-	}
-	
-	public static <T> void setObject(String key, T value, int ttlSeconds) {
-		String json = new Gson().toJson(value);
-		jedis.setex(key, ttlSeconds, json);
+	    return json != null ? JsonUtils.getGson().fromJson(json, type) : null;
 	}
 	
 	public static void close() {
